dd5d7f3376dd4bbb761e11cf4f5f1e59f7c19d1c
Tym17 <Tym17@users.noreply.github.com>
2018-08-12 09:44:44 +0200

    config toggleing + configuration

---
+++ main.js
@@ -0,0 +1,66 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const config = require('./package.json').config;
+
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    resizable: true,
+    center: true
+  });
+
+  // and load the index.html of the app.
+  let botconf = {};
+  try {
+      botconf = require('./botconf.json');
+      mainWindow.loadFile('player.html');
+
+  } catch (e)
+  {
+    mainWindow.loadFile('first.html');
+  }
+
+
+  let template = [
+      {
+          label: 'Options',
+          submenu: [
+              {
+                  label:  'Re-configure',
+                  click () {
+                    mainWindow.loadFile('first.html');
+                  }
+              }
+          ]
+      }
+  ];
+
+  let menu = Menu.buildFromTemplate(template);
+  mainWindow.setMenu(menu);
+
+  if (config.debugmode) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+}
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
